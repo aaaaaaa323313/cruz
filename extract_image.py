db4817486e822bb186c7c9cad164617571438c2f
Guanyu Gao <ggao001@e.ntu.edu.sg>
2017-01-24 11:46:57 +0800

    new

--- extract_image.py
+++ extract_image.py
@@ -4,12 +4,11 @@ import sys
 path = str(sys.argv[1])
 
 for f in os.listdir(path):
-    if os.path.isfile(f):
+    file_path = os.path.join(path, f)
+    if os.path.isfile(file_path):
         f_name, _ = f.split('.')
         image_path = os.path.join(path, f_name)
         os.mkdir(image_path)
-
-        file_path = os.path.join(path, f)
 
         cmd = "ffmpeg -i " + file_path + " -vf fps=0.2 " + image_path + '/' + f_name + "_" + "%04d.jpg -hide_banner"
         print cmd
