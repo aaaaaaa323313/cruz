1c6fb9d6094c5cb107f98d545be5a1258c7d4f32
guanyu gao <ggao001@e.ntu.edu.sg>
2017-01-29 18:16:56 +0800

    new files

--- extract_image.py
+++ extract_image.py
@@ -6,11 +6,16 @@ path = str(sys.argv[1])
 for f in os.listdir(path):
     file_path = os.path.join(path, f)
     if os.path.isfile(file_path):
+        print f
         f_name, _ = f.split('.')
         image_path = os.path.join(path, f_name)
-        os.mkdir(image_path)
+	try:
+        	os.mkdir(image_path)
+	except:
+		print 'The dictionary exists'
+                continue
 
-        cmd = "ffmpeg -i " + file_path + " -vf fps=0.2 " + image_path + '/' + f_name + "_" + "%04d.jpg -hide_banner"
+        cmd = "/home/zhz/bin/ffmpeg -i " + file_path + " -vf fps=0.2 " + image_path + '/' + f_name + "_" + "%04d.jpg -hide_banner"
         print cmd
         os.system(cmd)
 
---
+++ user_stat.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+
+path = str(sys.argv[1])
