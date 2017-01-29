949d61fc72e0f05c9dd02483e79ca6db965a6614
springlake <544282962@qq.com>
2017-01-29 03:50:57 -0800

    d

--- user_stat.py
+++ user_stat.py
@@ -7,11 +7,19 @@ def hasNumbers(inputString):
 path = str(sys.argv[1])
 file = open(path)
 
+dict = {}
+
 for line in file.readlines():
     items = line.split(' ')
     key   = ''
     for item in items:
         if hasNumbers(item) == False and len(item) > 0:
             key = key + item + ' '
-    print key
+
+    #print key
+    if dict.haskey(key) == True:
+        dict[key] += 1
+    else:
+        dict[key] = 1
+
 
