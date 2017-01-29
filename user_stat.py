5b2de5fc5450fbc3a2ed03f1bc838c0b98c9956a
springlake <544282962@qq.com>
2017-01-29 03:15:30 -0800

    dd

--- user_stat.py
+++ user_stat.py
@@ -1,9 +1,15 @@
 import os
 import sys
 
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
 
 path = str(sys.argv[1])
 file = open(path)
 
 for line in file.readlines():
-    print line.split(' ')
+    items = line.split(' ')
+    for item in items:
+        if hasNumbers(item) == True and len(item) > 0:
+            print item
+
