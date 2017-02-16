32200af00c25f42622627367b0dc61d776c54aed
Guanyu Gao <ggao001@e.ntu.edu.sg>
2017-02-16 16:09:09 +0800

    guanyu

--- video_curve.py
+++ video_curve.py
@@ -1,3 +1,25 @@
+import os
+import sys
+import math
+import pickle
 
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+path = str(sys.argv[1])
+file = open(path)
+
+pkl_user_f = open('user_1.pkl', 'rb')
+user_f     = pickle.load(pkl_user_f)
+
+
+for line in file.readlines():
+    items = line.split(' ')
+    key   = ''
+    for item in items:
+        if hasNumbers(item) == False and len(item) > 0:
+            key = key + item + '_'
+
+    print user_f[key]
 
 
