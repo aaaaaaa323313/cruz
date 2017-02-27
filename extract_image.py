081070e81ca39840ce6a9cdc4c968d664741549a
Guanyu Gao <ggao001@e.ntu.edu.sg>
2017-02-27 20:31:34 +0800

    new

--- extract_image.py
+++ extract_image.py
@@ -8,14 +8,15 @@ for f in os.listdir(path):
     if os.path.isfile(file_path):
         print f
         f_name, _ = f.split('.')
-        image_path = os.path.join(path, f_name)
+        image_path = os.path.join(path, f_name + '_2s')
 	try:
         	os.mkdir(image_path)
 	except:
+                print image_path
 		print 'The dictionary exists'
                 continue
 
-        cmd = "/home/zhz/bin/ffmpeg -i " + file_path + " -vf fps=0.2 " + image_path + '/' + f_name + "_" + "%04d.jpg -hide_banner"
+        cmd = "/home/guanyu/bin/ffmpeg -i " + file_path + " -vf fps=0.5 " + image_path + '/' + f_name + "_" + "%04d.jpg -hide_banner"
         print cmd
         os.system(cmd)
 
